844b6ef2ae2a11b4336a594c95999c3c4242f62b
ederxx <93275824+ederxx@users.noreply.github.com>
2025-05-28 12:25:15 -0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,6 @@
-// components/Logo.tsx
 const Logo = () => {
-    return (
-      <div className="text-center mt-6">
-        <p className="text-xs sm:text-sm font-bold text-gray-600">APOIO:</p>
-        <img src="/src/assets/itclogo.png" alt="Logo de apoio" className="mx-auto mt-2 w-20 sm:w-24 h-auto" />
-      </div>
-    );
-  };
-  
-  export default Logo;
+  return (
+    <img src="/assets/itclogo.png" alt="Logo de apoio" className="mx-auto mt-2 w-20 sm:w-24 h-auto" />
+  );
+};
+export default Logo;
